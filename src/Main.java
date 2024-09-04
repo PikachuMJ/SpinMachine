049f75c0d4005ee8588c4262047b8d04ee0cc52e
PikachuMJ <mj_redfox@outlook.de>
2024-09-04 11:29:07 +0200

    t

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+@SuppressWarnings("All")
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/VisualMachine.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+@SuppressWarnings("ALL")
+public class VisualMachine {
+    private JButton resetButton;
+    private JButton spinButton;
+
+    public VisualMachine() {
+        spinButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
