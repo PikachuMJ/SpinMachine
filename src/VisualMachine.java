2c4bc014d484d9ffbdf838c8679d79e749a2ba6f
PikachuMJ <mj_redfox@outlook.de>
2024-09-04 11:34:31 +0200

    t

--- src/VisualMachine.java
+++ src/VisualMachine.java
@@ -2,18 +2,33 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 @SuppressWarnings("ALL")
-public class VisualMachine {
+public class VisualMachine extends JFrame{
     private JButton resetButton;
     private JButton spinButton;
     private JLabel Machine;
     private JPanel SpinMachine;
 
+
     public VisualMachine() {
+
+        setContentPane(SpinMachine);
+        pack();
+        setTitle("Spin Machine");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
         spinButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
             }
         });
+
+        setVisible(true);
     }
 }
