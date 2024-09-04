b331c641cfdf63491592ec74253a4b5863ed4444
PikachuMJ <mj_redfox@outlook.de>
2024-09-04 12:18:35 +0200

    .

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,11 @@ public class Main {
     public static void main(String[] args) {
         new VisualMachine();
     }
+    /*  TODO   1. moving spins
+               2. point multiplier (bet more money)
+               3. automate spins
+               4. see the points + spins on screen
+               5. convert points to "money"
+               6. slot machine skins
+     */
 }
